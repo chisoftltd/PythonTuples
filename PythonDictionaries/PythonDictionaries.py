0b6500f09fee4e55718f48131b0bbbc8be4cc28f
chisoftltd <chisoftltd@gmail.com>
2021-02-19 22:48:15 +0100

    Python Dictionaries

--- PythonDictionaries/PythonDictionaries.py
+++ PythonDictionaries/PythonDictionaries.py
@@ -4,3 +4,35 @@ thisdict = {
   "year": 1964
 }
 print(thisdict)
+
+#Dictionary Items
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+#Duplicates Not Allowed
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+
+#Dictionary Length
+print(len(thisdict))
+
+#Dictionary Items - Data Types
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+print(thisdict)
+print(type(thisdict))
