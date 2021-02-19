2cf0e1bcb6784e2dcc12351dac16f0939e9ae91b
chisoftltd <chisoftltd@gmail.com>
2021-02-19 22:40:46 +0100

    Python - Join Sets

--- PythonSets/PythonSet.py
+++ PythonSets/PythonSet.py
@@ -58,3 +58,97 @@ mylist = ["kiwi", "orange"]
 thisset.update(mylist)
 
 print(thisset)
+
+#Python - Remove Set Items
+thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset)
+
+#Discard
+#Note: If the item to remove does not exist, discard() will NOT raise an error.
+thisset = {"apple", "banana", "cherry"}
+
+thisset.discard("banana")
+
+print(thisset)
+
+#pop()
+#Note: Sets are unordered, so when using the pop() method, you do not know which item that gets removed.
+thisset = {"apple", "banana", "cherry"}
+
+x = thisset.pop()
+
+print(x)
+
+print(thisset)
+
+#claer()
+thisset = {"apple", "banana", "cherry"}
+
+thisset.clear()
+
+print(thisset)
+
+#del
+thisset = {"apple", "banana", "cherry"}
+
+del thisset
+
+#print(thisset)
+
+#Python - Loop Sets
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+
+#Python - Join Sets
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+
+set3 = set1.union(set2)
+print(set3)
+
+set4 = set3.union(set2)
+print(set4)
+
+#update()
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+
+set1.update(set2)
+print(set1)
+
+#intersection_update()
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+x.intersection_update(y)
+
+print(x)
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.intersection(y)
+
+print(z)
+
+#symmetric_difference_update()
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+x.symmetric_difference_update(y)
+
+print(x)
+
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.symmetric_difference(y)
+
+print(z)
