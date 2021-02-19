012f443723b8ad8ccc495770b35c7cb02a3bf9b0
chisoftltd <chisoftltd@gmail.com>
2021-02-19 22:19:42 +0100

    Python Stes

--- PythonSets/PythonSet.py
+++ PythonSets/PythonSet.py
@@ -11,3 +11,11 @@ thisset = {"apple", "banana", "cherry"}
 
 print(len(thisset))
 
+#Set Items - Data Types
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
+print(set1)
+print(set2)
+print(set3)
