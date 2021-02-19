d1c85aa1a8c2de46d82c8496c84a88ba0fd0cc1f
chisoftltd <chisoftltd@gmail.com>
2021-02-19 22:28:53 +0100

    Python - Add Set Items

--- PythonSets/PythonSet.py
+++ PythonSets/PythonSet.py
@@ -19,3 +19,42 @@ set3 = {True, False, False}
 print(set1)
 print(set2)
 print(set3)
+
+print(type(set1))
+print(type(set2))
+print(type(set3))
+
+#Access Items
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset)
+print()
+
+#Python - Add Set Items
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+thisset.add("okwa")
+
+print(thisset)
+
+#Add Sets
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset)
+
+#Add Any Iterable
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
